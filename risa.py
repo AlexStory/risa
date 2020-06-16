9d6aadba632c93e35d3dec73cd6c2bbd4c3aceae
alex story <astory11@yahoo.com>
2020-06-16 17:16:44 -0500

    broke out math from string

--- risa.py
+++ risa.py
@@ -98,8 +98,9 @@ async def roll(ctx, *, n='100'):
                 logging.info(f'count: {count}')
                 logging.info(f'size: {size}')
                 logging.info(f'mod: {mod}')
+                total = sum(rolls) + mod
                 rolls = [str(r) for r in rolls]
-                await ctx.send(f'{sum(rolls) + mod} ({",".join(rolls)})')
+                await ctx.send(f'{total} ({",".join(rolls)})')
         except:
             await ctx.send('Bad input try like one of the following: `roll` `roll 6` `roll 2d6+3`')
 
