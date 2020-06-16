065940193a8598ac54ab7f0d22ee9fdb69b1d942
alex story <astory11@yahoo.com>
2020-06-16 17:10:28 -0500

    added parens to multi-rolls

--- risa.py
+++ risa.py
@@ -98,7 +98,7 @@ async def roll(ctx, *, n='100'):
                 logging.info(f'count: {count}')
                 logging.info(f'size: {size}')
                 logging.info(f'mod: {mod}')
-                await ctx.send(f'{sum(rolls) + mod}')
+                await ctx.send(f'{sum(rolls) + mod} ({",".join(rolls)}')
         except:
             await ctx.send('Bad input try like one of the following: `roll` `roll 6` `roll 2d6+3`')
 
